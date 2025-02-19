005634f709c3fcfe942f5ff2e2c764e6bf6a3dc7
mariakuleshova <2maria.kuleshova@gmail.com>
2025-02-19 12:48:15 +0300

    Power

--- SimpleCalculator/src/Calculator.java
+++ SimpleCalculator/src/Calculator.java
@@ -9,4 +9,26 @@
  */
 public class Calculator {
     
+    
+    public static double power(double a, double b) {
+        if (a == 0) {
+            if (b > 0) {
+                return 0;  
+            } else if (b < 0) {
+                throw new ArithmeticException();
+            } else {
+                return 1; 
+            }
+        }
+
+        if (a < 0 && (b % 1 != 0)) {
+            throw new ArithmeticException();  
+        }
+        
+        if(b == 0) {
+            return 1; 
+        }
+
+        return Math.pow(a, b);
+    }
 }
